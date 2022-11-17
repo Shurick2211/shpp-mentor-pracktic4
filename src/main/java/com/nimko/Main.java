8488f7b9e985f909273fd2378727e601b73d00c0
sn <shurick2211@gmail.com>
2022-11-17 19:08:25 +0200

    MainLogicServices

--- src/main/java/com/nimko/Main.java
+++ src/main/java/com/nimko/Main.java
@@ -1,36 +1,11 @@
 package com.nimko;
 
-import com.nimko.repo.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.nimko.services.MainLogicServices;
 
-import java.util.Scanner;
-
-/**
- * Hello world!
- *
- */
 public class Main{
-    private static final int NUM_STORES = 10;
-    private static final int NUM_PRODS = 3000000;
-    private static final String TYPE = "type";
 
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
     public static void main( String[] args ){
-        String sysProp = System.getProperty(TYPE);
-        CreateTables.createTables();
-        new CreateStore(NUM_STORES).addStoresInDb();
-        new CreateProductsAndAddToStore(NUM_STORES,NUM_PRODS)
-                .createProducts()
-                .addProductInStores();
-
-        log.info("It's address store, where maximal number products of entered type: {}",
-                 GetOperation.getAddress(sysProp == null ? getTypeFromConsole() : sysProp));
-    }
-
-    private static String getTypeFromConsole(){
-        Scanner scanner = new Scanner(System.in);
-        log.info("Enter type of product?");
-        return scanner.nextLine().trim();
+        MainLogicServices services = new MainLogicServices();
+        services.printResult();
     }
 }
---
+++ src/main/java/com/nimko/services/MainLogicServices.java
@@ -0,0 +1,37 @@
+package com.nimko.services;
+
+import com.nimko.repo.CreateProductsAndAddToStore;
+import com.nimko.repo.CreateStore;
+import com.nimko.repo.CreateTables;
+import com.nimko.repo.GetOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
+
+public class MainLogicServices {
+    private static final int NUM_STORES = 10;
+    private static final int NUM_PRODS = 3000000;
+    private static final String TYPE = "type";
+
+    private static final Logger log = LoggerFactory.getLogger(MainLogicServices.class);
+    public MainLogicServices() {
+        CreateTables.createTables();
+        new CreateStore(NUM_STORES).addStoresInDb();
+        new CreateProductsAndAddToStore(NUM_STORES,NUM_PRODS)
+                .createProducts()
+                .addProductInStores();
+    }
+
+    public void printResult(){
+        String sysProp = System.getProperty(TYPE);
+        log.info("It's address store, where maximal number products of entered type: {}",
+                GetOperation.getAddress(sysProp == null ? getTypeFromConsole() : sysProp));
+    }
+
+    private String getTypeFromConsole(){
+        Scanner scanner = new Scanner(System.in);
+        log.info("Enter type of product?");
+        return scanner.nextLine().trim();
+    }
+}
